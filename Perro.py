8d7f3e6522e8265ba6381a086d2ee10a41c95c86
Ysaiasbds <y.barroeta@outlook.com>
2025-04-04 10:57:24 +0200

    Delete Perro.py

--- Perro.py
+++
@@ -1,8 +0,0 @@
-from Animal import Animal
-
-class Perro(Animal):
-    def __init__(self, nombre, edad):
-        super().__init__(nombre, edad, "Perro")
-
-    def hacer_sonido(self):
-        return "Guau"
